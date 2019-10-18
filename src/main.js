395498678ea99a76299f768c318af464978bb3c9
lxynox <lxynox@gmail.com>
2019-10-18 22:34:12 +0800

    prettier setup

--- src/main.js
+++ src/main.js
@@ -6,9 +6,7 @@ import './main.css'
 
 // even though Rollup is bundling all your files together, errors and
 // logs will still point to your original source modules
-console.log(
-  'if you have sourcemaps enabled in your devtools, click on main.js:5 -->'
-)
+console.log('if you have sourcemaps enabled in your devtools, click on main.js:5 -->')
 
 update()
 
